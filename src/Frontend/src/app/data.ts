b8c55e4c7b5aa82873776aa8636de2d1f0f8c055
SirePi <yamailbox@gmail.com>
2021-10-07 14:22:35 +0200

    style

--- src/Frontend/src/app/data.ts
+++ src/Frontend/src/app/data.ts
@@ -1,14 +1,91 @@
 import { Article } from './article';
 
-export const ARTICLES: Article[] = [
-    { id: 11, name: 'Dr Nice', price: 1.45 },
-    { id: 12, name: 'Narco', price: 12.45 },
-    { id: 13, name: 'Bombasto', price: 1.45 },
-    { id: 14, name: 'Celeritas', price: 1.45 },
-    { id: 15, name: 'Magneta', price: 1.45 },
-    { id: 16, name: 'RubberMan', price: 1.45 },
-    { id: 17, name: 'Dynama', price: 1.45 },
-    { id: 18, name: 'Dr IQ', price: 1.45 },
-    { id: 19, name: 'Magma', price: 1.45 },
-    { id: 20, name: 'Tornado', price: 1.45 }
-];
+export const ARTICLES: Article[] = [{
+    "id": 134,
+    "name": "Brown eggs",
+    "price": 28.1,
+}, {
+    "id": 234,
+    "name": "Sweet fresh stawberry",
+    "price": 29.45,
+}, {
+    "id": 23,
+    "name": "Asparagus",
+    "price": 18.95,
+}, {
+    "id": 84,
+    "name": "Green smoothie",
+    "price": 17.68,
+}, {
+    "id": 432,
+    "name": "Raw legums",
+    "price": 17.11,
+}, {
+    "id": 1548,
+    "name": "Baking cake",
+    "price": 11.14,
+}, {
+    "id": 1837,
+    "name": "Pesto with basil",
+    "price": 18.19,
+}, {
+    "id": 8324,
+    "name": "Hazelnut in black ceramic bowl",
+    "price": 27.35,
+}, {
+    "id": 4054,
+    "name": "Fresh stawberry",
+    "price": 28.59,
+}, {
+    "id": 187,
+    "name": "Lemon and salt",
+    "price": 15.79,
+}, {
+    "id": 52,
+    "name": "Homemade bread",
+    "price": 17.48,
+}, {
+    "id": 545,
+    "name": "Legums",
+    "price": 14.77,
+}, {
+    "id": 485,
+    "name": "Fresh tomato",
+    "price": 16.3,
+}, {
+    "id": 935,
+    "name": "Healthy breakfast",
+    "price": 13.02,
+}, {
+    "id": 89,
+    "name": "Green beans",
+    "price": 28.79,
+}, {
+    "id": 19,
+    "name": "Baked stuffed portabello mushrooms",
+    "price": 20.31,
+}, {
+    "id": 18,
+    "name": "Strawberry jelly",
+    "price": 14.18,
+}, {
+    "id": 782,
+    "name": "Pears juice",
+    "price": 19.49,
+}, {
+    "id": 975,
+    "name": "Fresh pears",
+    "price": 15.12,
+}, {
+    "id": 684,
+    "name": "Caprese salad",
+    "price": 16.76,
+}, {
+    "id": 218,
+    "name": "Oranges",
+    "price": 21.48,
+}, {
+    "id": 9421,
+    "name": "Vegan food",
+    "price": 29.66,
+}]
