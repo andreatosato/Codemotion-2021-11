260ecd5df141b06a75fe2d0fb3f6ce7ed8e3a50e
Andrea Tosato <andrea.tosato@hotmail.it>
2021-10-17 17:44:53 +0200

    Update python and opentelemetry

--- src/PythonService/app.py
+++ src/PythonService/app.py
@@ -3,11 +3,42 @@ from flask import request
 from flask import jsonify, abort
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
+from opentelemetry import trace
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.exporter.zipkin.json import ZipkinExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.propagate import set_global_textmap
+from opentelemetry.propagators.b3 import B3MultiFormat
+
+trace.set_tracer_provider(
+    TracerProvider(
+        resource=Resource.create({SERVICE_NAME: "python-service"})
+    )
+)
+zipkin_exporter = ZipkinExporter(
+    # version=Protocol.V2
+    # optional:
+    endpoint="http://zipkin:9411/api/v2/spans",
+    # local_node_ipv4="192.168.0.1",
+    # local_node_ipv6="2001:db8::c001",
+    # local_node_port=31313,
+    # max_tag_value_length=256
+    # timeout=5 (in seconds)
+)
+trace.set_tracer_provider(TracerProvider())
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(zipkin_exporter)
+)
+tracer = trace.get_tracer(__name__)
+set_global_textmap(B3MultiFormat())
 
 app = Flask(__name__)
 FlaskInstrumentor().instrument_app(app, excluded_urls="client/.*/info,healthcheck")
-connectionString = "mssql+pymssql://sa:m1Password[12J@sqlserver/Store"
+RequestsInstrumentor().instrument()
+connectionString = "mssql+pyodbc://sa:m1Password[12J@sqlserver/Store?driver=ODBC+Driver+17+for+SQL+Server"
 app.config['SQLALCHEMY_DATABASE_URI'] = connectionString
 
 from models import db, Store, metadata
@@ -31,7 +62,7 @@ def GetAllStore():
 def GetProductAvailability(productId):
     result = db.session.query(Store).filter_by(productId=productId).one_or_none()
     if result:
-        return str(result)
+        return jsonify({"productId":result.productId, "availability":result.availability})
     else:
         abort(404)
 
