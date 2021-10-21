8541b98a67b4855b0071d8947b172d6605f64a94
Andrea Tosato <andrea.tosato@hotmail.it>
2021-10-21 20:10:10 +0200

    Sistemazione porte container. TODO: aggiornare il docker compose con i parametri

--- src/Aggregator/Program.cs
+++ src/Aggregator/Program.cs
@@ -3,19 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-//builder.Services.Configure<ForwardedHeadersOptions>(options =>
-//{
-//    options.ForwardLimit = 2;
-//    options.KnownProxies.Add(IPAddress.Parse("127.0.10.1"));
-//    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
-//});
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-//app.UseForwardedHeaders();
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
