4f13cc7d91371af43ed5d256d18b5892e00dba9d
Andrea Tosato <andrea.tosato@hotmail.it>
2021-11-06 19:34:57 +0100

    Clear Starter

--- src/DotNetService/ViewModels/ProductViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Orders.ViewModels;
-public class ProductViewModel
-{
-    public int ProductId { get; set; }
-    public int Quantity { get; set; }
-    public decimal SoldPrice { get; set; }
-}
